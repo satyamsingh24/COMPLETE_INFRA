d37376ea52f4f30151151360ea96b8340db141df
mayank <mayank.gudadhe@nenosystems.in>
2026-03-19 17:16:13 +0000

    a

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,25 +1,26 @@
-resource "aws_ecr_repository" "backend" {
-  name                 = "${var.project_name}-backend"
+resource "aws_ecr_repository" "app" {
+  name                 = "${var.project_name}-app"
   image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-
+  force_delete         = true
   tags = {
-    Name = "${var.project_name}-backend"
+    Name = "${var.project_name}-ecr"
+  }
+}
+
+resource "aws_ecr_repository" "backend" {
+  name                 = "${var.project_name}-app-backend"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+  tags = {
+    Name = "${var.project_name}-ecr-backend"
   }
 }
 
 resource "aws_ecr_repository" "nginx" {
-  name                 = "${var.project_name}-nginx"
+  name                 = "${var.project_name}-app-nginx"
   image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-
+  force_delete         = true
   tags = {
-    Name = "${var.project_name}-nginx"
+    Name = "${var.project_name}-ecr-nginx"
   }
 }
